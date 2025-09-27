46a142fbeb1c5b62ec033a59095801f72942158b
wamamili <wamamiliclean@gmail.com>
2025-09-27 19:52:47 +0300

    extending Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,12 +9,12 @@ interface Teacher {
 }
 
 // Extend Teacher interface for Directors
-interface Directors extends Teacher {
+interface Director extends Teacher {
     numberOfReports: number;
 }
 
 // Example usage
-const director1: Directors = {
+const director1: Director = {
     firstName: 'John',
     lastName: 'Doe',
     location: 'London',
