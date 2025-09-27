3f1d1aad550bc769de4cd3b592b561e9c62ddba3
wamamili <wamamiliclean@gmail.com>
2025-09-27 23:42:19 +0300

    Creating functions

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -53,7 +53,20 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
 }
 
+// Type predicate: checks if employee is Director
+function isDirector(employee: Director | Teacher): employee is Director {
+    return employee instanceof Director;
+}
+
+// Function that executes work depending on employee type
+function executeWork(employee: Director | Teacher): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
 // Example usage
-console.log(createEmployee(200));    // Teacher
-console.log(createEmployee(1000));   // Director
-console.log(createEmployee("$500")); // Director
+console.log(executeWork(createEmployee(200)));   // Getting to work
+console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
