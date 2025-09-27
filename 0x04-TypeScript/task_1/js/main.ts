dbfee1db977a1122f33af06a8f04ee0204a13658
wamamili <wamamiliclean@gmail.com>
2025-09-27 20:24:47 +0300

    Printing Teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -23,16 +23,17 @@ const director1: Director = {
 };
 
 console.log(director1);
-// Function implementation
-function printTeacher(firstName: string, lastName: string): string {
-    return `${firstName.charAt(0)}. ${lastName}`;
-}
 
 // Interface for the function
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
+// Function implementation using destructuring
+const printTeacher: printTeacherFunction = function ({ firstName, lastName }: { firstName: string; lastName: string }): string {
+    return `${firstName[0]}. ${lastName}`;
+};
+
 // Example usage
-console.log(printTeacher("John", "Doe"));
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
 // Output: J. Doe
